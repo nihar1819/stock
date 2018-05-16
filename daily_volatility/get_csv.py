4003523975dfe5f799791cc4454a89a85265deae
nsarangi <nihar1819@gmail.com>
2018-05-16 23:50:31 +0530

    initial stuff

--- daily_volatility/get_csv.py
+++ daily_volatility/get_csv.py
@@ -7,13 +7,13 @@ import os
 
 yesterday = (date.today() - timedelta(1)).strftime('%d%m%Y')
 VOLATILITY_FILE_NAME = "CMVOLT_{}.CSV".format(yesterday)
-VOLATILITY_FILE_PATH = r"./{}".format(VOLATILITY_FILE_NAME)
+VOLATILITY_FILE_PATH = r"../resources/{}".format(VOLATILITY_FILE_NAME)
 
 volatility_url = r'https://www.nseindia.com/archives/nsccl/volt/{}'.format(VOLATILITY_FILE_NAME)
 
 nifty_100_url = r'https://www.nseindia.com/content/indices/ind_nifty100list.csv'
 nifty_100_file_name = nifty_100_url.split('/')[-1]
-NIFTY_100_FILE_PATH = r"./{}".format(nifty_100_file_name)
+NIFTY_100_FILE_PATH = r"../resources/{}".format(nifty_100_file_name)
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -43,7 +43,6 @@ def download_file(url, output_file_path, check_file_presence=False):
 
 
 def get_deliverable_file_path():
-    #print (date.today() - timedelta(1)).strftime('%d%m%Y')
     # check if current time is already 3:30 pm or not
     if (datetime.now().time().hour == 15 and datetime.now().minute > 30) or datetime.now().time().hour > 15:
         date_str =  (date.today()).strftime('%d_%m_%Y')
@@ -51,12 +50,12 @@ def get_deliverable_file_path():
         date_str = (date.today() - timedelta(1)).strftime('%d_%m_%Y')
 
     file_name = "{}_{}.csv".format(date_str,'deleverables')
-    file_path = r'./{}'.format(file_name)
+    file_path = r'../resources/{}'.format(file_name)
     return file_path
 
 
 def get_csv():
-    download_file(volatility_url, VOLATILITY_FILE_NAME, True)
+    download_file(volatility_url, VOLATILITY_FILE_PATH, True)
     download_file(nifty_100_url, NIFTY_100_FILE_PATH, True)
 
     deliverable_url = r'https://www.nseindia.com/products/content/sec_bhavdata_full.csv'
